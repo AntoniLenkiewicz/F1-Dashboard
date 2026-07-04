321d5a536c77b09f7ff763abb288dbdf0a130b0a
AntoniLenkiewicz <antoniolenkiewicz2@gmail.com>
2026-07-04 14:53:36 +0100

    Rewrote functions to get standings, fixed bug when getting next event

--- src/python/models.py
+++ src/python/models.py
@@ -3,95 +3,76 @@ import fastf1
 from datetime import datetime, timezone
 from flask import Flask
 from fastf1.livetiming.client import SignalRClient
+import math
 
 def GetDriverStandings(season):
     schedule = fastf1.get_event_schedule(season, include_testing=False)
     standings = {}
-    short_event_names = []
     currentDate = datetime.now(timezone.utc)
     for _, event in schedule.iterrows():
-        event_name, round_number, sprint_start, race_start = event["EventName"], event["RoundNumber"], event["Session3DateUtc"], event["Session5DateUtc"]
-        short_event_names.append(event_name.replace("Grand Prix", "").strip())
-        if str(sprint_start) > str(currentDate):
-            break
+        eventName, sprintStart, raceStart, eventFormat = event["EventName"], event["Session3DateUtc"], event["Session5DateUtc"], event["EventFormat"]
 
-        # Load race results
-        if str(race_start) < str(currentDate):
-            race = fastf1.get_session(season, event_name, "R")
-            race.load(laps=False, telemetry=False, weather=False, messages=False)
-        
-        # Check for sprint race
-        sprint = None
-        if event["EventFormat"] == "sprint_qualifying":
-            sprint = fastf1.get_session(season, event_name, "S")
+        if eventFormat == "sprint_qualifying" and str(sprintStart) < str(currentDate):
+            sprint = fastf1.get_session(season, eventName, "S")
             sprint.load(laps=False, telemetry=False, weather=False, messages=False)
-        for _, driver_row in race.results.iterrows():
-            abbreviation, race_points, race_position = (
-                driver_row["Abbreviation"],
-                driver_row["Points"],
-                driver_row["Position"]
-            )
-            
-            sprint_points = 0
-            if sprint is not None:
-                driver_row = sprint.results[
-                    sprint.results["Abbreviation"] == abbreviation
-                ]
-                if not driver_row.empty:
-                    sprint_points = driver_row["Points"].values[0]
-            
+            for _, driverSprintData in sprint.results.iterrows():
+                if driverSprintData["Abbreviation"] not in standings:
+                    if math.isnan(driverSprintData["Points"]):
+                        driverSprintData["Points"] = 0
+                    standings[driverSprintData["Abbreviation"]] = driverSprintData["Points"]
+                else:
+                    if math.isnan(driverSprintData["Points"]):
+                        driverSprintData["Points"] = 0
+                    standings[driverSprintData["Abbreviation"]] = standings[driverSprintData["Abbreviation"]] + driverSprintData["Points"]
 
-            if abbreviation not in standings:
-                standings[abbreviation]=int(race_points + sprint_points)
-            
-            else:
-                standings[abbreviation] = standings[abbreviation] + int((race_points + sprint_points))
-    return standings
+        if str(raceStart) < str(currentDate):
+            race = fastf1.get_session(season, eventName, "R")
+            race.load(laps=False, telemetry=False, weather=False, messages=False)
+            for _, driverRaceData in race.results.iterrows():
+                if driverRaceData["Abbreviation"] not in standings:
+                    if math.isnan(driverRaceData["Points"]):
+                        driverRaceData["Points"] = 0
+                    standings[driverRaceData["Abbreviation"]] = driverRaceData["Points"]
+                else:
+                    if math.isnan(driverRaceData["Points"]):
+                        driverRaceData["Points"] = 0
+                    standings[driverRaceData["Abbreviation"]] = standings[driverRaceData["Abbreviation"]] + driverRaceData["Points"]
+    return (standings)
+
 
 def GetTeamStandings(season):
     schedule = fastf1.get_event_schedule(season, include_testing=False)
     standings = {}
-    short_event_names = []
     currentDate = datetime.now(timezone.utc)
     for _, event in schedule.iterrows():
-        event_name, round_number, sprint_start, race_start = event["EventName"], event["RoundNumber"], event["Session3DateUtc"], event["Session5DateUtc"]
-        short_event_names.append(event_name.replace("Grand Prix", "").strip())
-        if str(sprint_start) > str(currentDate):
-            break
+        eventName, sprintStart, raceStart, eventFormat = event["EventName"], event["Session3DateUtc"], event["Session5DateUtc"], event["EventFormat"]
 
-        # Load race results
-        if str(race_start) < str(currentDate):
-            race = fastf1.get_session(season, event_name, "R")
-            race.load(laps=False, telemetry=False, weather=False, messages=False)
-        
-        # Check for sprint race
-        sprint = None
-        if event["EventFormat"] == "sprint_qualifying":
-            sprint = fastf1.get_session(season, event_name, "S")
+        if eventFormat == "sprint_qualifying" and str(sprintStart) < str(currentDate):
+            sprint = fastf1.get_session(season, eventName, "S")
             sprint.load(laps=False, telemetry=False, weather=False, messages=False)
-        for _, driver_row in race.results.iterrows():
-            abbreviation, race_points, race_position, teamName = (
-                driver_row["Abbreviation"],
-                driver_row["Points"],
-                driver_row["Position"],
-                driver_row["TeamName"]
-            )
-            
-            sprint_points = 0
-            if sprint is not None:
-                driver_row = sprint.results[
-                    sprint.results["TeamName"] == teamName
-                ]
-                if not driver_row.empty:
-                    sprint_points = driver_row["Points"].values[0]
-            
+            for _, driverSprintData in sprint.results.iterrows():
+                if driverSprintData["TeamName"] not in standings:
+                    if math.isnan(driverSprintData["Points"]):
+                        driverSprintData["Points"] = 0
+                    standings[driverSprintData["TeamName"]] = driverSprintData["Points"]
+                else:
+                    if math.isnan(driverSprintData["Points"]):
+                        driverSprintData["Points"] = 0
+                    standings[driverSprintData["TeamName"]] = standings[driverSprintData["TeamName"]] + driverSprintData["Points"]
 
-            if teamName not in standings:
-                standings[teamName]=int(race_points + sprint_points)
-            
-            else:
-                standings[teamName] = standings[teamName] + int((race_points + sprint_points))
-    return standings
+        if str(raceStart) < str(currentDate):
+            race = fastf1.get_session(season, eventName, "R")
+            race.load(laps=False, telemetry=False, weather=False, messages=False)
+            for _, driverRaceData in race.results.iterrows():
+                if driverRaceData["TeamName"] not in standings:
+                    if math.isnan(driverRaceData["Points"]):
+                        driverRaceData["Points"] = 0
+                    standings[driverRaceData["TeamName"]] = driverRaceData["Points"]
+                else:
+                    if math.isnan(driverRaceData["Points"]):
+                        driverRaceData["Points"] = 0
+                    standings[driverRaceData["TeamName"]] = standings[driverRaceData["TeamName"]] + driverRaceData["Points"]
+    return (standings)
 
 # Will need updating later to return whether the event is currently underway
 def GetNextEvent():
@@ -102,9 +83,10 @@ def GetNextEvent():
     for _, event in schedule.iterrows():
         for i in range(1,6):
             if str(event[f"Session{i}DateUtc"]) > str(currentDate):
-                return {"eventName": event["EventName"],"eventType" : event["EventFormat"], "eventRoundNumber" : event["RoundNumber"] }
+                print(event)
+                return {"eventName": event["EventName"],"eventType" : event[(f"Session{i}")], "eventRoundNumber" : event["RoundNumber"] }
 
-
+GetNextEvent()
 # Trial for getting live data
 
 # from fastf1.livetiming.client import SignalRClient
